349e123186746ab35b06d8927f1a365d47d2cbcf
Hara Chung <harachung90@gmail.com>
2022-12-16 20:00:40 +1100

    Second commit. Some more comments

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import React from 'react';
   //const isGoingOut = true
   //let answer = isGoingOut ? "Yes" : "No"
 
+     /* Toggles the value of isGoingOut when clicked (false -> true, true -> false */
   function toggleState() {
+      // Otherwise, setIsGoingOut(prevState => !prevState) also does the same thing!
       setIsGoingOut(!isGoingOut)
   }
   return (
       <div className="state">
         <h1 className="state--title">Do I feel like going out tonight?</h1>
-        <div className="state--value" onClick={toggleState}>
+
+        <div className="state--value"
+             onClick={toggleState}
+        >
           {/*  This way, no need for the answer variable on line 8 */}
           <h1>{isGoingOut ? "Yes" : "No"}</h1>
         </div>
